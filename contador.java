7756f48d73bfb4c8d5465b8a20a1529e0029f305
JulioMartins853 <jm4120194@gmail.com>
2025-05-02 15:29:26 -0300

    Add files via upload

---
+++ contador.java
@@ -0,0 +1,13 @@
+public class contador {
+    public static void main(String[] args) {
+
+        int contador = 0;
+
+        while( contador < 5){
+            System.out.println("contador: " + contador);
+            contador++;
+        }
+        
+    }
+    
+}
---
+++ contadorcomfor.java
@@ -0,0 +1,9 @@
+public class contadorcomfor {
+    public static void main(String[] args) {
+        
+        for(int contador = 0; contador < 5; contador++){
+            System.out.println("contador: " + contador);
+        }
+    }
+    
+}
